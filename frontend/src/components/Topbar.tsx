7d628f961ee41cfaadf9532d60c3a2b239b48aec
achyut_amrutia <achyutamrutia@gmail.com>
2025-12-19 22:33:20 +0530

    Done all the necessary changes

--- frontend/src/components/Topbar.tsx
+++ frontend/src/components/Topbar.tsx
@@ -17,8 +17,8 @@ const Topbar = () => {
     '
 		>
 			<div className='flex gap-2 items-center'>
-				<img src='/spotify.png' className='size-8' alt='Spotify logo' />
-				Spotify
+				<img src='/voxo.png' className='size-8' alt='Voxo logo' />
+				Voxo
 			</div>
 			<div className='flex items-center gap-4'>
 				{isAdmin && (
